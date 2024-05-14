b0a4816c23052e4c97ba428a1ce99c951722bf82
Arihant Tripathi <tarihant2001@gmail.com>
2024-05-14 13:01:11 -0400

    Docker Poppler

--- llama2.py
+++ llama2.py
@@ -156,9 +156,9 @@ def load_data_llama2():
     # with open(file_path_2, 'r') as file:
     #     json_data_2  = json.load(file)
 
-    # Extract links and append to the existing array
-    new_links = [item['link'] for item in json_data]
-    articles.extend(new_links)
+    # # Extract links and append to the existing array
+    # new_links = [item['link'] for item in json_data]
+    # articles.extend(new_links)
 
     # # Iterate through the dictionary and extend the existing list with the links
     # for letter, links in json_data_2.items():
